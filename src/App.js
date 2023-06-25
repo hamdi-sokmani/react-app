f73d51861289865f5f6d067be4269533d42fb235
Hamdi Sokmani <hamdisokmani@gmail.com>
2023-06-25 21:55:18 +0200

    Added home and about pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import HelloWorld  from './Components/HelloWorld';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import Home from './Views/Home';
+import About from './Views/About';
 
 function App() {
   return (
     <div>
-      <Header />
-      
-      <HelloWorld name="Hamdi"/>
-
-      <Footer />
+      <Router>
+        <Header />
+          <div className="p-3">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+            </Routes>
+          </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,42 +1,64 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useTransition, animated } from 'react-spring';
+import NavigationMenu from "./NavigationMenu";
 
 function Navigation() {
     const [showMenu, setShowMenu] = React.useState(false);
 
-    let menu;
-    let menuMask;
+    const maskTransitions = useTransition(showMenu, {
+        from:  { opacity: 0, position: 'absolute'},
+        enter: { opacity: 1 },
+        leave: { opacity: 0 },
+    })
 
-    if(showMenu) {
-        menu = 
-        <div
-            className="fixed bg-white top-0 left-0 w-4/5 h-full z-50 shadow"
-        > 
-            The Menu
-        </div>
-
-        menuMask =
-        <div
-            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
-            onClick={() => setShowMenu(false)}
-        >
-
-        </div>
-    }
+    const menuTransitions = useTransition(showMenu, {
+        from : { opacity: 0, transform: 'translateX(-100%)' },
+        enter: { opacity: 1, transform: 'translateX(0%)' },
+        leave: { opacity: 0, transform: 'translateX(-100%)' },
+    })
 
     return (
         <nav>
             <span className="text-xl">
                 <FontAwesomeIcon 
-                    icon={faBars} 
+                    icon={faBars}
                     onClick={() => setShowMenu(!showMenu)}
                 />
             </span>
 
-            {menuMask}
-        
-            {menu}
+            {
+                maskTransitions((props, item, key) => {
+                    return (
+                        item &&
+                        <animated.div 
+                            key={key}   
+                            style={props}
+                            className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
+                            onClick={() => setShowMenu(false)}
+                        >
+                        </animated.div>
+                    );
+                  })
+            }
+
+            {
+                menuTransitions((props, item, key) => {
+                    return (
+                        item &&
+                        <animated.div 
+                            key={key}   
+                            style={props}
+                            className="bg-white fixed top-0 left-0 w-4/5 h-full z-50 shadow p-3"
+                        >
+                            <NavigationMenu 
+                                closeMenu={() => setShowMenu(false)}
+                            />
+                        </animated.div>
+                    );
+                })                
+            }
         </nav>
     );
 }
